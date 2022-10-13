74eb2567a94a3f404ba0852d3827eda3dfd80f24
Kenneth Lee <Kenneth-Lee-2012@qq.com>
2022-10-13 18:07:35 +0800

    typo fix
    Signed-off-by: Kenneth Lee <Kenneth-Lee-2012@qq.com>

--- source/conf.py
+++ source/conf.py
@@ -78,7 +78,7 @@ release = 'Release 3'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh'
+# language = 'zh'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
